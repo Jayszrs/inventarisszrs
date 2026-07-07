1f75c6a1898ccf90b29e48b0e50b9c1eab382e08
Jayszrs <jaelanisuryasaputra@gmail.com>
2026-07-07 14:04:28 +0700

    Use rounded favicon with white background

--- frontend/layout/header.php
+++ frontend/layout/header.php
@@ -8,8 +8,9 @@ $pageTitle = $pageTitle ?? APP_NAME;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= h($pageTitle) ?></title>
-    <link rel="icon" type="image/png" href="<?= h(url_path('/assets/img/logo-fazmastone-favicon.png')) ?>">
-    <link rel="apple-touch-icon" href="<?= h(url_path('/assets/img/logo-fazmastone-favicon.png')) ?>">
+    <link rel="icon" type="image/png" href="<?= h(url_path('/assets/img/logo-fazmastone-favicon-rounded.png?v=2')) ?>">
+    <link rel="shortcut icon" type="image/png" href="<?= h(url_path('/assets/img/logo-fazmastone-favicon-rounded.png?v=2')) ?>">
+    <link rel="apple-touch-icon" href="<?= h(url_path('/assets/img/logo-fazmastone-favicon-rounded.png?v=2')) ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800;900&family=Outfit:wght@400;600;700;800;900&display=swap" rel="stylesheet">
